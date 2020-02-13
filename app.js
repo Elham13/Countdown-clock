a993894b682bad036a77efb9029b5f97e873157a
Elhamuddin <60072659+Elham13@users.noreply.github.com>
2020-02-13 22:55:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+setInterval(() => {
+  var val = document.getElementById("date_input").value;
+  var countDate = new Date(val).getTime();
+  var now = new Date().getTime();
+  gap = countDate - now;
+
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+
+  var d = Math.floor(gap / day);
+  var h = Math.floor((gap % day) / hour);
+  var m = Math.floor((gap % hour) / minute);
+  var s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+}, 1000);
